5ba8fcc32b39306f99dccad0d3a872355a9382fa
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-08 02:21:11 +0300

    Fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 require('dotenv').config({path: __dirname + '/.env'});
 const Discord = require('discord.js');
 require('./database/connect_db');
-const client = new Discord.Client({partials: ['MESSAGE', 'REACTION', 'MESSAGE_DELETE']});
+const client = new Discord.Client({partials: ['MESSAGE']});
 const {registerCommands, registerEvents, registerHandlers} = require('./registry');
+console.log("Starting Random Botsons...");
 
 (async() => {
-    await client.login(process.env.BOT_TOKEN);
+    await client.login(process.env.TOKEN);
+    console.log("Logged in");
     client.commands = new Map();
     client.handlers = new Map();
     await registerEvents(client, './events');
