1fe42d805bc4a7437166bc182e1bbefcf8c1accd
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-09 19:03:30 +0300

    Added database connection

---
+++ src/database/connect_db.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_LINK,
+    {
+        useNewUrlParser: true,
+        useFindAndModify: false,
+        useUnifiedTopology: true
+    });
