7fac318ae6482240fb94aad385a29168c5fa1d7d
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-10 20:32:08 +0300

    Finished dice command

--- src/commands/dice.js
+++ src/commands/dice.js
@@ -1,14 +1,19 @@
 const embed = require('discord.js').MessageEmbed;
 const Stats = require('../database/models/stats');
+const {MessageAttachment} = require('discord.js');
 
 module.exports = {
     run: async(client, message) => {
         let roll = Math.floor((Math.random() * 6) + 1);
+        let attachment = new MessageAttachment(`./src/images/dice/face-${roll}.png`, `face-${roll}.png`);
+
         let rollEmbed = new embed()
             .setColor(0xFCFCFC)
             .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.avatarURL())
             .setTitle("Rolled the dice")
             .setDescription(`Dice: ${roll}`)
+            .attachFiles(attachment)
+            .setImage(`attachment://face-${roll}.png`)
         await message.channel.send(rollEmbed);
 
         try {
@@ -21,5 +26,5 @@ module.exports = {
         }
     },
     aliases: ["roll"],
-    description: ""
+    description: "rolls the dice"
 }
