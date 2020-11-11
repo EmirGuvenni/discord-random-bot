6f6dcfeb17a5604bed5cb15c10354af363d5acfd
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-11 20:47:25 +0300

    Added select command

---
+++ src/commands/select.js
@@ -0,0 +1,29 @@
+const embed = require('discord.js').MessageEmbed;
+const Stats = require('../database/models/stats');
+
+module.exports = {
+    run: async (client, message, args) => {
+        // Get a random argument index
+        let selection = Math.floor(Math.random() * args.length);
+
+        // Create an embed
+        let selectEmbed = new embed()
+            .setColor(0xFCFCFC)
+            .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.avatarURL())
+            .setTitle("Selected")
+            .setDescription(`Selection: **${args[selection]}**`);
+        await message.channel.send(selectEmbed);
+
+        //  Save stats
+        try {
+            const reqStats = await Stats.findOne();
+            reqStats.select++;
+            reqStats.save();
+        }
+        catch(err) {
+            client.handlers.get("error")(client, err, __filename);
+        }
+    },
+    aliases: [],
+    description: "selects a random argument"
+}
