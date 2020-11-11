9a041da1b3ad833b7b1bd42a1a786dd9b3c747d6
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-12 02:45:35 +0300

    Added invite command

---
+++ src/commands/invite.js
@@ -0,0 +1,25 @@
+const embed = require('discord.js').MessageEmbed;
+const Stats = require('../database/models/stats');
+
+module.exports = {
+    run: async (client, message) => {
+        // Create the invite embed
+        let invEmbed = new embed()
+            .setColor(0xFCFCFC)
+            .setTitle("You could invite me from here.")
+            .setDescription(process.env.HOMEPAGE);
+        await message.channel.send(invEmbed);
+
+        //  Save stats
+        try {
+            const reqStats = await Stats.findOne();
+            reqStats.invite++;
+            reqStats.save();
+        }
+        catch(err) {
+            client.handlers.get("error")(client, err, __filename);
+        }
+    },
+    aliases: [],
+    description: "send an invite link"
+}
