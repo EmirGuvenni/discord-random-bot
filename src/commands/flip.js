298c84472420a0c3d16928a3aa1493084a9e4e10
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-12 21:30:40 +0300

    Added comments and made flipping shorter

--- src/commands/flip.js
+++ src/commands/flip.js
@@ -4,21 +4,25 @@ const {MessageAttachment} = require('discord.js');
 
 module.exports = {
     run: async (client, message) => {
-        let flip = Math.random() <= 0.5;
+        // Flip a coin
         let coin;
-        (flip) ? coin = "heads" : coin = "tails";
+        (Math.random() <= 0.5) ? coin = "heads" : coin = "tails";
 
+        // Get the coin image
         let attachment = new MessageAttachment(`./src/images/coin/${coin}.png`, `${coin}.png`);
 
+        // Create an embed
         let rollEmbed = new embed()
             .setColor(0xFCFCFC)
             .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.avatarURL())
             .setTitle("Flipped a coin")
             .setDescription(`Coin: ${coin}`)
             .attachFiles(attachment)
-            .setImage(`attachment://${coin}.png`)
+            .setImage(`attachment://${coin}.png`);
+        // Send the embed
         await message.channel.send(rollEmbed);
 
+        // Save stats
         try {
             const reqStats = await Stats.findOne();
             reqStats.flip++;
