3a962240ab7f2011cb4660757b661fb55a2b1378
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-12 17:32:36 +0300

    Added comments

--- src/handlers/error.js
+++ src/handlers/error.js
@@ -3,6 +3,7 @@ const embed = require('discord.js').MessageEmbed;
 
 module.exports = {
     run: async (client, err, file) => {
+        // Save stats
         try{
             const reqStats = await Stats.findOne();
             reqStats.errs++;
@@ -12,6 +13,7 @@ module.exports = {
             client.channels.cache.get(process.env.ERROR_CHANNEL).send( __filename + "\n" +err);
         }
 
+        // Send the error stack to development server
         client.channels.cache.get(process.env.ERROR_CHANNEL).send(embed.debug(file, err.stack));
     },
     description: "reports errors to the developer server"
