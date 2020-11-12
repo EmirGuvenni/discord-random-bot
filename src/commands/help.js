ac5f8daa79f896831c14852ba6f6f1ce76978955
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-12 21:36:59 +0300

    Updated commands and added comments
    Fixed the error handler as well

--- src/commands/help.js
+++ src/commands/help.js
@@ -3,27 +3,29 @@ const Stats = require('../database/models/stats');
 
 module.exports = {
     run: async(client, message) => {
+        // Create an embed
         let helpEmbed = new embed()
             .setColor(0xFCFCFC)
             .setTitle("Command list")
             .setDescription([
-                "**/dice:** Rolls a 6 face dice.",
-                "**/flip:** Flips a coins.",
-                "**/random:** ",
-                "**/range:** Returns a random number between the range user has given.",
-                "**/select:** Returns a random argument from the array",
-                "**/card:** Returns a random card."
+                "**card:** Picks a random card.",
+                "**dice:** Rolls the dice.",
+                "**flip:** Flips a coins.",
+                "**invite:** Sends a link to bots homepage",
+                "**range:** Returns a random number between the range user has given.",
+                "**select:** Picks a random argument from the array"
             ]);
-
+        // Send the embed
         await message.channel.send(helpEmbed);
 
+        // Save stat
         try {
             const reqStats = await Stats.findOne();
             reqStats.help++;
             reqStats.save();
         }
         catch(err) {
-            client.handlers.get("error")(client, message, err, __filename);
+            client.handlers.get("error")(client, err, __filename);
         }
     },
     aliases: ["commands"],
