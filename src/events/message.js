df007c4f55da71525f658cf15bcabeb1fa35f5f7
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-12 19:23:14 +0300

    Added commands and made prefix dynamic-ish

--- src/events/message.js
+++ src/events/message.js
@@ -1,10 +1,14 @@
 module.exports = async(client, message) => {
-    if(!message.content.startsWith("/random"))
+    // Return if it's not a command
+    if(!message.content.startsWith(process.env.PREFIX))
         return;
 
+    // Get message arguments
     const args = message.content.slice(8).trim().split(/ +/);
+    // Get the command argument
     const command = args.shift().toLowerCase();
 
+    // Check if the command exists
     if(client.commands.get(command))
         return client.commands.get(command)(client, message, args);
 }
