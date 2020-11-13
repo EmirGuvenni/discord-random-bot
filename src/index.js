8cb8149251553140e7dc34842ea37ebad0c67e23
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-13 03:47:14 +0300

    Moved ready event into index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 require('./database/connect_db');
 const client = new Discord.Client({partials: ['MESSAGE']});
 const {registerCommands, registerEvents, registerHandlers} = require('./registry');
+const Stats = require('./database/models/stats');
 console.log("Starting Random Botsons...");
 
 (async() => {
@@ -13,4 +14,17 @@ console.log("Starting Random Botsons...");
     await registerEvents(client, './events');
     await registerCommands(client, './commands');
     await registerHandlers(client, './handlers');
+    // Set bot activity
+    await client.user.setActivity("/random help");
+    // Create a stats document if doesn't already exists
+    if(!await Stats.findOne()){
+        try{
+            const newStats = new Stats();
+            newStats.save();
+            console.log("Created new stats document");
+        }
+        catch(err) {
+            client.handlers.get("error")(client, err, __filename);
+        }
+    }
 })();
