d561a2e9d8f803c1972248691a4c2d7b2a24b490
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-13 04:17:27 +0300

    Added comments

--- src/events/guildCreate.js
+++ src/events/guildCreate.js
@@ -1,8 +1,10 @@
 const embed = require('discord.js').MessageEmbed;
 
 module.exports = async(client, guild) => {
+    // Get the guild owner
     let owner = await client.users.cache.get(guild.ownerID);
 
+    // Create the developer embed
     let joinEmbed = new embed()
         .setColor(0xFCFCFC)
         .setTitle(guild.name)
@@ -11,8 +13,10 @@ module.exports = async(client, guild) => {
             `**Owner ID:** ${owner.id}`,
             `**Owner tag:** ${owner.username}#${owner.discriminator}`
         ])
+    // Send the embed to the developer server
     client.channels.cache.get(process.env.JOIN_CHANNEL).send(joinEmbed);
 
+    // Create the client embed
     let newGuildEmbed = new embed()
         .setColor(0xFCFCFC)
         .setTitle(`Hello ${owner.username}!`)
@@ -20,5 +24,6 @@ module.exports = async(client, guild) => {
             'To get started use the "/random help" command or flip a coin with "/random flip"!',
             `For more info, please visit ${process.env.HOMEPAGE}`
         ]);
+    // Send the client embed to the guild owner
     await owner.send(newGuildEmbed);
 }
