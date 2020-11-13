aca2450f198cd7c8cf3e694e0c96973dfd1420b1
Emir Güvenni <45740926+EmirGuvenni@users.noreply.github.com>
2020-11-13 04:09:16 +0300

    Added number command

---
+++ src/commands/number.js
@@ -0,0 +1,52 @@
+const embed = require('discord.js').MessageEmbed;
+const Stats = require('../database/models/stats');
+
+module.exports = {
+    run: async(client, message, args) => {
+        let num;
+        // Check if there's a provided number
+        if(!args[0]){
+            num = Math.floor((Math.random() * 10) + 1);
+            // Create a warning embed
+            let checkEmbed = new embed()
+                .setColor(0xFCFCFC)
+                .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.avatarURL())
+                .setTitle("Picked a number")
+                .setDescription(`Number: **${num}**`);
+            // Send the embed
+            return message.channel.send(checkEmbed);
+        }
+        else if(isNaN(args[0])){
+            // Create a warning embed
+            let warnEmbed = new embed()
+                .setColor(0xFCFCFC)
+                .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.avatarURL())
+                .setTitle("Error")
+                .setDescription("The provided argument must be a number.");
+            // Send the embed
+            return message.channel.send(warnEmbed);
+        }
+        num = Math.floor((Math.random() * args[0]) + 1);
+
+        // Create an embed
+        let checkEmbed = new embed()
+            .setColor(0xFCFCFC)
+            .setAuthor(`${message.author.username}#${message.author.discriminator}`, message.author.avatarURL())
+            .setTitle("Picked a number")
+            .setDescription(`Number: **${num}**`);
+        // Send the embed
+        await message.channel.send(checkEmbed);
+
+        //  Save stats
+        try {
+            const reqStats = await Stats.findOne();
+            reqStats.number++;
+            reqStats.save();
+        }
+        catch(err) {
+            client.handlers.get("error")(client, err, __filename);
+        }
+    },
+    aliases: ["int"],
+    description: "picks a random number between 1 and the provided number or 10"
+}
